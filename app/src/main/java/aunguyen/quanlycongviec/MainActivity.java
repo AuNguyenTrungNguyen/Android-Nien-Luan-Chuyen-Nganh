3d06260ae3a84c1b89ab8b919dc0c8f74866e43c
aunguyentrungnguyen <nguyen06111996@gmail.com>
2018-02-06 14:43:39 +0700

    Connect to Firebase

--- app/src/main/java/aunguyen/quanlycongviec/MainActivity.java
+++ app/src/main/java/aunguyen/quanlycongviec/MainActivity.java
@@ -1,7 +1,10 @@
 package aunguyen.quanlycongviec;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
     }
 }
